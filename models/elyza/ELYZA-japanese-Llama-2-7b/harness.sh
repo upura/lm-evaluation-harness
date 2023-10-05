5bda3dc216ba83bdf7fe46d72642cad942441d96
Naoki Orii <mrorii@gmail.com>
2023-10-05 08:23:15 +0000

    Add harnesses for ELYZA-japanese-Llama-2-7b

---
+++ models/elyza/ELYZA-japanese-Llama-2-7b/harness.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+MODEL_ARGS="pretrained=elyza/ELYZA-japanese-Llama-2-7b,torch_dtype=auto"
+TASK="jcommonsenseqa-1.1-0.2,jnli-1.1-0.2,marc_ja-1.1-0.2,jsquad-1.1-0.2,jaqket_v2-0.2-0.2,xlsum_ja,xwinograd_ja,mgsm"
+NUM_FEW_SHOTS="3,3,3,2,1,1,0,5"
+python main.py \
+    --model hf-causal \
+    --model_args $MODEL_ARGS \
+    --tasks $TASK \
+    --num_fewshot $NUM_FEW_SHOTS \
+    --device "cuda" \
+    --verbose \
+    --output_path "models/elyza/ELYZA-japanese-Llama-2-7b/result.json"
